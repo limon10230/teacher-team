9797b7c475cfac4fbe9722e49300d625d4eb7444
limon10230 <limon.dew121@gmail.com>
2021-09-26 22:15:41 +0600

    update commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHandHoldingUsd, faUser } from '@fortawesome/free-solid-svg-icons'
 import './Cart.css'
 
 const Cart = (props) => {
+    const icon = <FontAwesomeIcon icon={faUser} />
     const { cart } = props;
     let total = 0;
     for (const teachers of cart) {
         total = total + teachers.salary;
     }
+    let teacherName = [];
+    for (const teachers of cart) {
+        teacherName = teacherName + teachers.name;
+    }
     return (
         <div>
-            <h2>Teacher Added:{props.cart.length}</h2>
+            <h2>{icon} Teachers Added:{props.cart.length}</h2>
             <h2>Total Cost:${total}</h2>
+            <h2>Added Teachers name:{teacherName}</h2>
+
         </div>
     );
 };
--- src/components/Header/Heeader.js
+++ src/components/Header/Heeader.js
@@ -4,7 +4,7 @@ import './Heeader.css'
 const Heeader = () => {
     return (
         <div className="header">
-            <h1>Make a top Teacher team </h1>
+            <h1>Make a top Teachers team </h1>
             <p>A teacher, also called a schoolteacher or formally an educator, is a person who helps students to acquire knowledge, competence or virtue.</p>
             <h1>Total Budget: 100 Million</h1>
         </div>
